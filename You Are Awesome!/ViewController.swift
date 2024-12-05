9ab52ed7cbfbae2a993f070e73dc691be7243a5d
Wayne Gregory <wjg315@yahoo.com>
2024-12-05 07:10:56 -0600

    aa

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -24,18 +24,22 @@ class ViewController: UIViewController {
     }
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
+        let awesomeMessage = "You Are Awesome!"
+        let greatMessage = "You Are Great!"
+        let bombMessage = "You Are Da Bomb!"
         
-        if messageLabel.text == "You Are Awesome!" {
-            messageLabel.text = "You Are Great!"
+        if messageLabel.text == awesomeMessage {
+            messageLabel.text = greatMessage
             imageView.image = UIImage(named: "image1")
+        } else if messageLabel.text == greatMessage{
+            messageLabel.text = bombMessage
+            imageView.image = UIImage(named: "image2")
         } else {
-            messageLabel.text = "You Are Awesome!"
+            messageLabel.text = awesomeMessage
             imageView.image = UIImage(named: "image0")
         }
-        
-        
-        
-        
     }
     
+    
 }
+
